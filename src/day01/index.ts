51b0cd473dadb369d53cb71281ea72665feecebf
Leonardo Montini <lion.48m@gmail.com>
2023-12-01 19:44:05 +0100

    Add day 1

--- src/day01/index.ts
+++ src/day01/index.ts
@@ -1,17 +1,51 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => rawInput;
+const parseInput = (rawInput: string) => rawInput.split("\n");
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  const sum = input
+    .map((line) => {
+      const m = line.match(/[0-9]/g);
+
+      return Number(`${m.at(0)}${m.at(-1)}`);
+    })
+    .reduce((acc, curr) => acc + curr, 0);
+
+  return sum;
 };
+
+const numbers = [
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  const sum = input
+    .map((line) => {
+      const parsedLine = numbers.reduce((acc, curr, i) => {
+        return acc.replaceAll(curr, `${curr[0]}${i + 1}${curr.at(-1)}`);
+      }, line);
+
+      const m = parsedLine.match(
+        /([0-9]|one|two|three|four|five|six|seven|eight|nine)/g,
+      );
+
+      return Number(`${m.at(0)}${m.at(-1)}`);
+    })
+    .reduce((acc, curr) => acc + curr, 0);
+
+  return sum;
 };
 
 run({
